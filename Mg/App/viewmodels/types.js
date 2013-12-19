fa5d31143e232191d8de632628a0ae0f4094396d
Will T <rballonline@gmail.com>
2013-12-18 16:09:00 -0800

    Eod

--- Mg/App/viewmodels/types.js
+++ Mg/App/viewmodels/types.js
@@ -1,14 +1,5 @@
 ﻿define(['knockout'], function (ko) {
 	var self = this;
-	self.typeSelected = ko.observable(false);
-	self.type = ko.observable();
-
-	self.activate = function (type) {
-		if (type) {
-			self.typeSelected(true);
-			self.type(type);
-		}
-	};
 
 	return self;
 });
